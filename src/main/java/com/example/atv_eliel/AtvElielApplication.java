02c0dfefd71c5cbd822ed77e53e7c6bb4efbf36c
devgabs1 <gabriel.fe132@gmail.com>
2026-03-12 19:27:53 -0300

    first commit

---
+++ src/main/java/com/example/atv_eliel/AtvElielApplication.java
@@ -0,0 +1,13 @@
+package com.example.atv_eliel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtvElielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtvElielApplication.class, args);
+	}
+
+}
