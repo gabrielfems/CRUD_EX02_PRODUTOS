e05d5929884363fd7a36442cebe0de9bebb0d14d
devgabs1 <gabriel.fe132@gmail.com>
2026-03-23 20:06:29 -0300

    Criando Classe Controller

--- src/main/java/com/example/atv_eliel/controller/ProdutoController.java
+++ src/main/java/com/example/atv_eliel/controller/ProdutoController.java
@@ -9,6 +9,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/produto")
@@ -17,13 +18,19 @@ public class ProdutoController {
     @Autowired
     private ProdutoService produtoService;
 
-    @GetMapping("/listar")
+    @GetMapping
     public ResponseEntity <List<ProdutoModel>> findAll() {
         List<ProdutoModel> request = produtoService.listarProdutos();
         return ResponseEntity.ok().body(request);
     }
 
-    @PostMapping("/cadastro")
+    @GetMapping("/{id}")
+    public ResponseEntity exibeProduto(@PathVariable Long id) {
+        Optional<ProdutoModel> produto = produtoService.listarProduto(id);
+        return ResponseEntity.ok().body(produto);
+    }
+
+    @PostMapping
     public ResponseEntity <ProdutoModel> create(@RequestBody ProdutoModel produto) {
         ProdutoModel request = produtoService.cadastrarProduto(produto);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
@@ -31,13 +38,13 @@ public class ProdutoController {
         return ResponseEntity.created(uri).body(request);
     }
 
-    @PatchMapping("/atualizar")
+    @PatchMapping("/{id}")
     public ResponseEntity <ProdutoModel> update(@PathVariable Long id, @RequestBody ProdutoModel produto) {
         ProdutoModel request = produtoService.atualizarProduto(id, produto);
         return ResponseEntity.ok().body(request);
     }
 
-    @DeleteMapping("/deletar")
+    @DeleteMapping("/{id}")
     public ResponseEntity <ProdutoModel> delete(@PathVariable Long id) {
         produtoService.deletarProduto(id);
         return ResponseEntity.noContent().build();
